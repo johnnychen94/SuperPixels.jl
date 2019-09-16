1f2765c3ec30f62a65a9b2dc07ceae3fbc1fa7bd
JohnnyChen <johnnychen94@hotmail.com>
2019-09-16 13:13:12 +0800

    Files generated by PkgTemplates

---
+++ src/SuperPixels.jl
@@ -0,0 +1,5 @@
+module SuperPixels
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SuperPixels
+using Test
+
+@testset "SuperPixels.jl" begin
+    # Write your own tests here.
+end
